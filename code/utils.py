f46c9fa41bca381a49a40305bdf34bcb20c1b51f
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2022-10-04 13:52:59 -0400

    more reorg

--- code/utils.py
+++ code/utils.py
@@ -1,2 +1,3 @@
-DATA_DIR = "~/projects/singlecell-kaggle/data"
+DATA_DIR = "~/projects/singlecell-kaggle/data/original/"
+SPARSE_DATA_DIR = "~/projects/singlecell-kaggle/data/sparse/"
 OUTPUT_DIR = "~/projects/singlecell-kaggle/data/submissions"
