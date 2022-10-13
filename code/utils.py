000710c3902b21a72c6eb227c1fe2c4f9bb3d016
Lucas Schiefelbein <luke.schiefelbein@gmail.com>
2022-10-13 15:42:57 +0200

    Revert "Jasotron pyenv"

--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,3 @@
-from genericpath import isdir
-from multiprocessing.sharedctypes import Value
-import os
-import pathlib
-
 import numpy as np
 import pandas as pd
 import logging
@@ -10,34 +5,13 @@ from dataclasses import dataclass
 
 logging.basicConfig(level=logging.INFO)
 
-
-def get_git_root():
-    last_cwd = os.getcwd()
-    while not os.path.isdir('.git'):
-        os.chdir('..')
-        cwd = os.getcwd()
-        if cwd == last_cwd:
-            raise OSError('no .git directory')
-        last_cwd = cwd
-    return last_cwd
-
-
-# Original data.
-DATA_DIR = pathlib.Path(get_git_root()) / "data" / "original"
-
-# Sparse data dir.
-SPARSE_DATA_DIR = pathlib.Path(get_git_root()) / "data" / "sparse"
-
-# Output data dir.
-OUTPUT_DIR = pathlib.Path(get_git_root()) / "data" / "submissions"
-
-for path in [DATA_DIR, SPARSE_DATA_DIR, DATA_DIR]:
-    if not path.is_dir():
-        raise ValueError(f"directory not found: f{path}")
+DATA_DIR = "/Users/luke/projects/singlecell-kaggle/data/original"
+SPARSE_DATA_DIR = "/Users/luke/projects/singlecell-kaggle/data/sparse"
+OUTPUT_DIR = "/Users/luke/projects/singlecell-kaggle/data/submissions"
 
 
 @dataclass
-class ExperimentRepository:
+class Technology:
     name: str
 
     def __post_init__(self):
@@ -51,26 +25,22 @@ class ExperimentRepository:
         self.train_targets_sparse_idxcol_path: str = f"{SPARSE_DATA_DIR}/train_{self.name}_targets_idxcol.npz"
         self.test_inputs_sparse_idxcol_path: str = f"{SPARSE_DATA_DIR}/test_{self.name}_inputs_idxcol.npz"
 
-# TODO: rename
-multi = ExperimentRepository("multi")
-cite = ExperimentRepository("cite")
+
+multi = Technology("multi")
+cite = Technology("cite")
 
 
-def format_submission(Y_pred_raw, repo: ExperimentRepository):
+def format_submission(Y_pred_raw, tech: Technology):
     """
     Takes a square matrix of `gene*cell` of the kind usually output
     from models and formats it as necessary for submission to the 
     kaggle competition
     """
     logging.info('Loading indices...')
-    test_index = np.load(
-        repo.test_inputs_sparse_idxcol_path,
-        allow_pickle=True,
-    )["index"]
-    y_columns = np.load(
-        repo.train_targets_sparse_idxcol_path,
-        allow_pickle=True,
-    )["columns"]
+    test_index = np.load(tech.test_inputs_sparse_idxcol_path,
+                         allow_pickle=True)["index"]
+    y_columns = np.load(tech.train_inputs_sparse_idxcol_path,
+                        allow_pickle=True)["columns"]
 
     # Maps from row number to cell_id
     cell_dict = dict((k, i) for i, k in enumerate(test_index))
--- papers_and_notes/Journal.md
+++ papers_and_notes/Journal.md
@@ -2,10 +2,6 @@
 
 Probably biggest blocker to making another submission with the RBF kernel is local CV. Only problem is CV is very expensive, and it's hard/expensive to even train one model. The public kaggle notebooks seem to just downsample aggressively, and the RBF solution from last year did this as well. 
 
-Paired with @jasotron today and did a lot of code refactoring. 
-
-Today need to 1) merge his branch, 2) finalize a system to keep track of performance locally (submit via api and centralize experiment data)
-
 # 10/11
 
 Today I'd like to get one of the winners from last year working. Their approach was simple-ish, and was for CITE data, which is both easier to work with and more important for scoring. Code here: https://github.com/openproblems-bio/neurips2021_multimodal_topmethods/tree/main/src/predict_modality/methods/Guanlab-dengkw/run
