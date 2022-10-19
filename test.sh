3f0bb4e59675cbf9ff9e42f42065a8df34647767
Luke Schiefelbein <luke.schiefelbein@gmail.com>
2022-10-18 21:39:05 -0400

    added proper logging and testing using prefect pipeline

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ cd "$(git rev-parse --show-toplevel)"
 cd code
 # Will not work on windows as dumps temporary notebook to /dev/null	
 # See https://github.com/nteract/papermill/issues/405
-papermill  Last\ Year\ RNA-\>Prot\ Test.ipynb /dev/null -p IS_TEST True --log-output
+papermill last_year_rbf_prefect.ipynb /dev/null -p IS_TEST True --log-output
